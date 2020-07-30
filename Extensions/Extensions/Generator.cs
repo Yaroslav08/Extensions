68d76007fa36a1a5b4eeccc853166b0773e1e65d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-07-30 19:17:48 +0300

    Released GetCode method

--- Extensions/Extensions/Generator.cs
+++ Extensions/Extensions/Generator.cs
@@ -7,8 +7,21 @@ namespace Extensions
     {
         public static int GetCode(int length)
         {
-
+            var chars = "0123456789";
+            var intCodes = new int[length];
+            var random = new Random();
+            for (int i = 0; i < intCodes.Length; i++)
+            {
+                intCodes[i] = chars[random.Next(chars.Length)];
+            }
+            string result = null;
+            for (int i = 0; i < intCodes.Length; i++)
+            {
+                result += intCodes[i];
+            }
+            return Convert.ToInt32(result);
         }
+
         public static string GetString(int length, bool IsUpper = false, bool IsLowwer = false)
         {
             var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
