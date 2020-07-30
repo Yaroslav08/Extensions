22fb2f6df79437a6483ee3266f5ef959a3b43b38
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-07-30 18:31:11 +0300

    Added library and added some helper methods

--- Extensions/Extensions/PasswordManager.cs
+++ Extensions/Extensions/PasswordManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 namespace Extensions
 {
@@ -15,9 +17,35 @@ namespace Extensions
 
         }
 
-        private static byte[] ComposePasswordHash(byte[] salt, uint iterationCount, byte[] passwordHash)
+        private byte[] ComposeIdentityV3Hash(byte[] salt, uint iterationCount, byte[] passwordHash)
         {
+            var hash = new byte[1 + 4 + 4 + 4 + salt.Length + 32];
+            hash[0] = 1;
+            Buffer.BlockCopy(ConvertToNetworkOrder((uint)KeyDerivationPrf.HMACSHA256), 0, hash, 1, sizeof(uint));
+            Buffer.BlockCopy(ConvertToNetworkOrder((uint)iterationCount), 0, hash, 1 + sizeof(uint), sizeof(uint));
+            Buffer.BlockCopy(ConvertToNetworkOrder((uint)salt.Length), 0, hash, 1 + 2 * sizeof(uint), sizeof(uint));
+            Buffer.BlockCopy(salt, 0, hash, 1 + 3 * sizeof(uint), salt.Length);
+            Buffer.BlockCopy(passwordHash, 0, hash, 1 + 3 * sizeof(uint) + salt.Length, passwordHash.Length);
+            return hash;
+        }
+        private bool AreByteArraysEqual(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length) return false;
 
+            var areEqual = true;
+            for (var i = 0; i < array1.Length; i++)
+            {
+                areEqual &= (array1[i] == array2[i]);
+            }
+            return areEqual;
+        }
+        private byte[] ConvertToNetworkOrder(uint number)
+        {
+            return BitConverter.GetBytes(number).Reverse().ToArray();
+        }
+        private uint ConvertFromNetworOrder(byte[] reversedUint)
+        {
+            return BitConverter.ToUInt32(reversedUint.Reverse().ToArray(), 0);
         }
     }
 }
