a83cd948431b32b1ddecc48473d8f4dff7ac476a
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-23 13:27:41 +0200

    Added Tracker for change state elements

---
+++ Extensions/Extensions/Tracker/Object.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Extensions.Tracker
+{
+    public class Object<T>
+    {
+        public DateTime CreatedAt { get; set; }
+        public string ObjectId { get; set; }
+        public List<Obj> Objs { get; set; }
+        public T OldState { get; set; }
+        public T NewState { get; set; }
+    }
+}
---
+++ Extensions/Extensions/Tracker/Tracker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace Extensions.Tracker
+{
+    public class Tracker<T> where T : class
+    {
+        private List<Object<T>> objects;
+        public Tracker()
+        {
+            objects = new List<Object<T>>();
+        }
+        public Tracker(string content)
+        {
+            objects = new List<Object<T>>();
+            objects = JsonSerializer.Deserialize<List<Object<T>>>(content);
+        }
+
+        public string Save()
+        {
+            return JsonSerializer.Serialize(objects);
+        }
+        public void Upload(string content)
+        {
+            objects = JsonSerializer.Deserialize<List<Object<T>>>(content);
+        }
+
+        public void Clear()
+        {
+            objects.Clear();
+        }
+
+        public void Add(Object<T> element)
+        {
+            if (element == null)
+                throw new NullReferenceException(nameof(element));
+            if (objects == null)
+                throw new NullReferenceException(nameof(objects));
+            objects.Add(element);
+        }
+        public void Remove(Object<T> element)
+        {
+            if (element == null)
+                throw new NullReferenceException(nameof(element));
+            if (objects == null)
+                throw new NullReferenceException(nameof(objects));
+            objects.Remove(element);
+        }
+
+        public async void ImportFromFile(string filePath)
+        {
+            using var sr = new StreamReader(filePath);
+            Clear();
+            objects = JsonSerializer.Deserialize<List<Object<T>>>(await sr.ReadToEndAsync());
+            sr.Close();
+            sr.Dispose();
+        }
+        public void ExportToFile(string path, string fileName)
+        {
+            using var sw = new StreamWriter(path + fileName);
+            sw.WriteAsync(JsonSerializer.Serialize(objects));
+            sw.Close();
+            sw.Dispose();
+        }
+        public Object<T> GetFirstElement() => objects.FirstOrDefault();
+        public Object<T> GetLastElement() => objects.LastOrDefault();
+    }
+}
