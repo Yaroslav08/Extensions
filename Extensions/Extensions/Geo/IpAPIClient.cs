8688b5cba7b41fad4669ce5d7465796cc40742a0
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-23 12:35:11 +0200

    Added IP functionality

---
+++ Extensions/Extensions/Geo/IpAPIClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Extensions.Geo
+{
+    public abstract class IpAPIClient
+    {
+        private const string BaseUrl = "http://ip-api.com/json/";
+        private static HttpClient httpClient = new HttpClient();
+
+        public static async Task<IPGeo> GetFullInfo(string ip)
+        {
+            if (ip == null) throw new ArgumentNullException(nameof(ip));
+            httpClient.BaseAddress = new Uri(BaseUrl);
+            httpClient.Timeout = TimeSpan.FromSeconds(20);
+            var resposne = await httpClient.GetAsync($"{ip}?fields=66846719");
+            return JsonSerializer.Deserialize<IPGeo>(await resposne.Content.ReadAsStringAsync());
+        }
+
+        public static async Task<IPGeo> GetShortInfo(string ip)
+        {
+            if (ip == null) throw new ArgumentNullException(nameof(ip));
+            httpClient.BaseAddress = new Uri(BaseUrl);
+            httpClient.Timeout = TimeSpan.FromSeconds(20);
+            var response = await httpClient.GetAsync($"{ip}?fields=18014429");
+            return JsonSerializer.Deserialize<IPGeo>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
